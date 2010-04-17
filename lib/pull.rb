1c40fa85ef7df8728b19f1c2d28ed58c8c522677
David Cabo <david@calibea.com>
2010-04-17 21:16:58 +0200

    first statement parsed!

--- lib/pull.rb
+++ lib/pull.rb
@@ -18,15 +18,67 @@ def parse_statement_trigger(t, attrs)
   attrs[:event_date] = t.search("tr:nth-child(2) .col5Cont:nth-child(2)").children.last.text
 end
 
-def parse_assets
+def parse_amount(s)
+  s.strip.gsub(/[^0-9,]+/i, '')
 end
 
-def parse_liabilities
+def parse_assets(t)
+  assets = t.children.last.children # TD inside the second TR  
+  # Parse the actual financial data, which comes as text + embedded tables
+  expecting = ''
+  assets.children.each do |line|
+    case line.text.strip
+    when "":
+      # Do nothing
+    when /^Bienes/:
+      expecting = :property_data
+    when /^CARACTERÍSTICAS/:
+      # TODO: Do nothing? part of case above always?
+    when /^Saldo total de cuentas bancarias/:
+      expecting = :bank_account_data
+    when /^\302/:
+      puts "GOT CASH #{parse_amount(line.text)}" if ( expecting == :bank_account_data )
+    else
+      if (line.node_name == 'table' and expecting == :property_data)
+        # The HTML is broken, so we can't fetch the <tr>. use TD instead, brute force...
+        line.search("td").each do |td|
+          puts "GOT PROPERTY #{parse_amount(td.text)}" if td.text =~ /\d/
+        end
+      else
+        puts "## #{line.text}"
+      end
+    end
+  end  
+end
+
+def parse_liabilities(t)
+  puts "Parsing liabilities"
+  liabilities = t.children.last.children
+  # Parse the actual financial data, which comes as text + embedded tables
+  liabilities.children.each do |line|
+    case line.text.strip
+    when "":
+      # Do nothing
+    when /VALOR EUROS/:
+      # Nothing to do
+    else
+      if (line.node_name == 'table')
+        # The HTML is broken, so we can't fetch the <tr>. use TD instead, brute force...
+        line.search("td").each do |td|
+          puts "OWES #{parse_amount(td.text)}" if td.text =~ /\d/
+        end
+      else
+        puts "## #{line.text}"
+      end
+    end
+  end  
 end
 
 def parse_financial_statement(t, attrs)
   t.search("table.info3").each do |section|
-    if ( section.text =~ /^ACTIVO/ )
+    puts "ERROR!! WTF is this? #{section}" if section.children.size != 2
+    
+    if ( section.children.first.text =~ /^ACTIVO/ )
       parse_assets(section) 
     elsif ( section.text =~ /^PASIVO/ )
       parse_liabilities(section) 
