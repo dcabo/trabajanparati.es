4591303492ee8d4fe496e8fb0d85544aaa3144e6
David Cabo <david@calibea.com>
2010-04-18 20:27:38 +0200

    let rails cache the whole site

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -1,10 +1,11 @@
 class PoliticiansController < ApplicationController
+  caches_page :index, :show
+  
   def index
     @politicians = Politician.find(:all, :order => "name asc")
   end
 
   def show
     @politician = Politician.find(params[:id])
-    
   end
 end
